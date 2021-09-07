815b99a5e83614f0329f6413abf15ad22c4013c6
Erick Alencar <alencar.erick12@gmail.com>
2021-09-07 17:47:22 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,14 @@
+function Footer() {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+        <div className="container">
+          <p className="text-light">App desenvolvido por <a href="https://github.com/ErickAlencar09" target="_blank" rel="noreferrer">Erick Alencar</a></p>
+          <p className="text-light"><small><strong>Semana Spring React</strong><br/>
+            Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+            <p className="text-light"> Contato <a href="https://www.linkedin.com/in/erick-alencar-306b67175/" target="_blank" rel="noreferrer">LinkedIn </a></p>
+        </div>
+      </footer>
+    );
+}
+
+export default Footer;
